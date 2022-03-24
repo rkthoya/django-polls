a24fd1301b0153ae4a62d2cb37f7225f9f22c221
Richard Thoya <rkthoya@gmail.com>
2022-03-24 19:37:14 +0300

    More simple views.

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.polls_app_index, name='polls_index')
+    path('', views.polls_app_index, name='polls_index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results', views.results, name='results'),
+    path('<int:question_id>/vote', views.vote, name='vote'),
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,16 @@ from django.http import HttpResponse
 
 def polls_app_index(request):
     return HttpResponse("Hello. You're at polls' index!")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
