36b2f276ae8ab71c286578ee2ad93f0f7fccde66
Richard Thoya <rkthoya@gmail.com>
2022-03-24 20:18:52 +0300

    Views using templates.

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def polls_app_index(request):
-    return HttpResponse("Hello. You're at polls' index!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
